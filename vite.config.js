a06a5bf028a1444f069d5ac2ea9306866b7cdd27
ZhangJiaxiang <zj.x9872@foxmail.com>
2023-12-08 11:02:54 +0800

    🐎 ci: split other chunks

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          lodash: ['lodash-es'],
+          echarts: ['echarts'],
+        },
+      },
+    },
+    target: ['es2022', 'edge110', 'firefox110', 'chrome110', 'safari16'],
+  },
 })
